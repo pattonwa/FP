c1bd321cab2995f0a1aec775f4601e8004a63877
pattonwa <patton.wang@centling.com>
2016-01-18 13:05:43 +0800

    This commit impletments: 1. The main activity with menu and fragments. 2. The image slide box in game god fragment.

--- app/src/main/java/com/centling/findplayer/MainActivity.java
+++ app/src/main/java/com/centling/findplayer/MainActivity.java
@@ -1,13 +1,101 @@
 package com.centling.findplayer;
-
-import android.support.v7.app.AppCompatActivity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private String TAG = "MainActivity";
+    private LinearLayout tabGameGod;
+    private LinearLayout tabExplore;
+    private LinearLayout tabMessage;
+    private LinearLayout tabMine;
+    private ImageView ivGameGod;
+    private ImageView ivExplore;
+    private ImageView ivMessage;
+    private ImageView ivMine;
+    private GameGodFragment fgGameGod;
+    private ExploreFragment fgExplore;
+    private MessageFragment fgMessage;
+    private MineFragment fgMine;
+    private FragmentManager fm;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //initial tab linearLayout
+        tabGameGod = (LinearLayout)findViewById(R.id.tab_game_god);
+        tabExplore = (LinearLayout)findViewById(R.id.tab_explore);
+        tabMessage = (LinearLayout)findViewById(R.id.tab_message);
+        tabMine = (LinearLayout)findViewById(R.id.tab_mine);
+        tabGameGod.setOnClickListener(this);
+        tabExplore.setOnClickListener(this);
+        tabMessage.setOnClickListener(this);
+        tabMine.setOnClickListener(this);
+
+        //initial image view
+        ivGameGod = (ImageView)findViewById(R.id.iv_game_god);
+        ivExplore = (ImageView)findViewById(R.id.iv_explore);
+        ivMessage = (ImageView)findViewById(R.id.iv_message);
+        ivMine = (ImageView)findViewById(R.id.iv_mine);
+
+        //set default fragment
+        fm = getFragmentManager();
+        fgGameGod = new GameGodFragment();
+        setFragment(fgGameGod);
     }
+
+    public void onClick(View v) {
+        initTabs();
+        switch (v.getId()) {
+            case R.id.tab_game_god:
+                ivGameGod.setImageResource(R.drawable.game_god_on);
+                setFragment(fgGameGod);
+                break;
+            case R.id.tab_explore:
+                if(fgExplore == null){
+                    fgExplore = new ExploreFragment();
+                }
+                ivExplore.setImageResource(R.drawable.explore_on);
+                setFragment(fgExplore);
+                break;
+            case R.id.tab_message:
+                if(fgMessage == null){
+                    fgMessage = new MessageFragment();
+                }
+                ivMessage.setImageResource(R.drawable.message_on);
+                setFragment(fgMessage);
+                break;
+            case R.id.tab_mine:
+                if (fgMine == null){
+                    fgMine = new MineFragment();
+                }
+                ivMine.setImageResource(R.drawable.mine_on);
+                setFragment(fgMine);
+                break;
+            default:
+                    Log.e(TAG, "Wrong tab id is received");
+        }
+    }
+
+    private void setFragment(Fragment newFg){
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.content, newFg);
+        ft.commit();
+    }
+
+    private void initTabs(){
+        ivGameGod.setImageResource(R.drawable.game_god);
+        ivExplore.setImageResource(R.drawable.explore);
+        ivMessage.setImageResource(R.drawable.message);
+        ivMine.setImageResource(R.drawable.mine);
+    }
+
 }
---
+++ app/src/main/java/com/centling/findplayer/customview/ImageSlideView.java
@@ -0,0 +1,240 @@
+package com.centling.findplayer.customview;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.centling.findplayer.R;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+
+/**
+ * Created by pattonwa on 2016/1/14.
+ */
+public class ImageSlideView extends FrameLayout{
+    private final String TAG = "ImageSlideView";
+    private Context context;
+    private ImageLoader imageLoader = ImageLoader.getInstance();
+    private List<ImageView> imageViewList;
+    private List<View> dotViewsList;
+    private String[] imageUrls;
+    private final static boolean isAutoPlay = true;
+    private ScheduledExecutorService scheduledExecutorService;
+    private ViewPager viewPager;
+    private int currentItem = 0;
+    private Handler handler = new Handler(){
+        @Override
+        public  void handleMessage(Message msg){
+            super.handleMessage(msg);
+            Log.v(TAG, "handler set the image");
+            viewPager.setCurrentItem(currentItem);
+        }
+    };
+
+    public ImageSlideView(Context context){
+        this(context, null);
+    }
+    public ImageSlideView(Context context, AttributeSet attrs){
+        this(context, attrs, 0);
+    }
+    public ImageSlideView(Context context, AttributeSet attrs, int defStyle){
+        super(context, attrs, defStyle);
+        this.context = context;
+        initImageLoader(context);
+        initData();
+
+        if(isAutoPlay){
+            Log.v(TAG,"auto play to start");
+            startPlay();
+        }
+    }
+
+    private void startPlay(){
+        scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+        Log.v(TAG, "startPlay:" + scheduledExecutorService.toString());
+        scheduledExecutorService.scheduleAtFixedRate(new SlideShowTask(), 1, 4, TimeUnit.SECONDS);
+    }
+
+    private void stopPlay(){
+        scheduledExecutorService.shutdown();
+    }
+
+    private class SlideShowTask implements Runnable{
+        @Override
+        public void run(){
+            synchronized (viewPager){
+                currentItem = (currentItem+1)%imageViewList.size();
+                handler.obtainMessage().sendToTarget();
+            }
+        }
+    }
+
+    public static void initImageLoader(Context context){
+        ImageLoaderConfiguration imgConfig = new ImageLoaderConfiguration.Builder(context).threadPriority(Thread.NORM_PRIORITY).build();
+        ImageLoader.getInstance().init(imgConfig);
+    }
+
+    private void initData(){
+        imageViewList = new ArrayList<ImageView>();
+        dotViewsList = new ArrayList<View>();
+        new GetListTask().execute("");
+    }
+
+    class GetListTask extends AsyncTask<String, Integer, Boolean>{
+        @Override
+        protected Boolean doInBackground(String... params){
+            try {
+                imageUrls = new String[]{
+                        "http://image.zcool.com.cn/56/35/1303967876491.jpg",
+                        "http://image.zcool.com.cn/59/54/m_1303967870670.jpg",
+                        "http://image.zcool.com.cn/47/19/1280115949992.jpg",
+                        "http://image.zcool.com.cn/59/11/m_1303967844788.jpg"
+                };
+                return true;
+            }catch ( Exception e){
+                e.printStackTrace();
+                return false;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result){
+            super.onPostExecute(result);
+            if(result){
+                initUI(context);
+            }
+        }
+    }
+    private void initUI(Context context){
+        if(imageUrls == null || imageUrls.length == 0){
+            Log.e(TAG, "There is no image urls");
+            return;
+        }
+        LayoutInflater.from(context).inflate(R.layout.image_slide_box,this,true);
+        LinearLayout dotLayout = (LinearLayout)findViewById(R.id.imageIndic);
+        dotLayout.removeAllViews();
+
+        for (int i = 0; i < imageUrls.length; i++){
+            ImageView view = new ImageView(context);
+            view.setTag(imageUrls[i]);
+            switch (i){
+                case 0:
+                    view.setBackgroundResource(R.drawable.image_slide_default);
+                    break;
+                case 1:
+                    view.setImageResource(R.drawable.tmp1);
+                    break;
+                case 2:
+                    view.setImageResource(R.drawable.tmp2);
+                    break;
+                case 3:
+                    view.setImageResource(R.drawable.tmp3);
+                    break;
+                default:
+                    view.setImageResource(R.drawable.tmp4);
+                    break;
+            }
+            view.setScaleType(ImageView.ScaleType.FIT_XY);
+            imageViewList.add(view);
+
+            ImageView dotView = new ImageView(context);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+            params.leftMargin = 4;
+            params.rightMargin = 4;
+            dotLayout.addView(dotView, params);
+            dotViewsList.add(dotView);
+        }
+        viewPager = (ViewPager)findViewById(R.id.viewPager);
+        viewPager.setFocusable(true);
+        viewPager.setAdapter(new MyPagerAdaper());
+        viewPager.setOnPageChangeListener(new MyPageChangeListener());
+    }
+
+    private class MyPageChangeListener implements ViewPager.OnPageChangeListener{
+        boolean isAutoPlay = false;
+
+        @Override
+        public void onPageScrollStateChanged(int arg0){
+            switch (arg0){
+                case 1:   //scrolling
+                    isAutoPlay = false;
+                    Log.v(TAG, "scrolling");
+                    break;
+                case 2:  //switching page
+                    isAutoPlay = true;
+                    Log.v(TAG, "swithing page");
+                    break;
+                case 0: //done
+                    if(viewPager.getCurrentItem() == viewPager.getAdapter().getCount()-1 && !isAutoPlay){
+                        viewPager.setCurrentItem(0);
+                    }
+                    else if(viewPager.getCurrentItem() == 0 && !isAutoPlay){
+                        viewPager.setCurrentItem(viewPager.getAdapter().getCount() - 1);
+                    }
+                    Log.v(TAG, "Done");
+                    break;
+            }
+        }
+
+        @Override
+        public void onPageSelected(int pos){
+            currentItem = pos;
+            for(int i=0; i < dotViewsList.size(); i++){
+                if (i == pos){
+                    ((View)dotViewsList.get(i)).setBackgroundResource(R.drawable.dot_on);
+                }else{
+                    ((View)dotViewsList.get(i)).setBackgroundResource(R.drawable.dot_off);
+                }
+            }
+        }
+
+        @Override
+        public void onPageScrolled(int arg0, float arg1, int arg2){
+            // TODO
+        }
+
+    }
+
+    private class MyPagerAdaper extends PagerAdapter{
+        @Override
+        public void destroyItem(View container, int position, Object object){
+            ((ViewPager)container).removeView(imageViewList.get(position));
+        }
+
+        @Override
+        public Object instantiateItem(View container, int position){
+            ImageView imageView = imageViewList.get(position);
+//            imageLoader.displayImage(imageView.getTag() + "", imageView);
+            ((ViewPager)container).addView(imageView);
+            return imageView;
+        }
+
+        @Override
+        public int getCount(){
+            return imageViewList.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(View arg0, Object arg1){
+            return arg0 == arg1;
+        }
+    }
+}
